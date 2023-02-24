1dd8e0f4fbb9d456d53d253dd1e939801e933bcd
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-02-25 04:29:28 +0530

    dsa sheet

---
+++ src/Sorting/selectionSort.java
@@ -0,0 +1,36 @@
+package Sorting;
+public class selectionSort {
+    public static void mySelectionSort(int []array)
+    {
+        for(int i=0 ; i<array.length-1 ; i++)
+        {
+            int min = i;
+            for(int j=i+1 ; j<array.length ; j++)
+            {
+                if(array[min]>array[j])
+                {
+                    min = j;
+                }
+            }
+
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void display(int [] array)
+    {
+        for(int i=0 ; i<array.length ; i++)
+        {
+            System.out.print(array[i]+"\t");
+        }
+    }
+    public static void main(String[] args) {
+        int []array = {5,4,1,3,2};
+        display(array);
+        System.out.println("");
+        mySelectionSort(array);
+        display(array);
+    }
+}
