917378b7ace570b02db9226da073ce7a0b0bf86d
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-05 21:48:09 +0530

    dsa sheet

---
+++ src/Array/maxSubArraySum.java
@@ -0,0 +1,27 @@
+//java program to find the maximum sub array sum
+package Array;
+public class maxSubArraySum {
+    public static int getMaxSum(int []array)
+    {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0 ; i<array.length ; i++)
+        {
+            for(int j=i ; j<array.length ; j++)
+            {
+                currentSum = 0;
+                for(int k = i ; k<=j ; k++)
+                {
+                    currentSum += array[k];
+                }
+                maxSum = Math.max(currentSum , maxSum);
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String [] args)
+    {
+        int []array = {2,4,6,8,10};
+        System.out.println("The max sum of the Sub-Array is "+getMaxSum(array));
+    }
+}
