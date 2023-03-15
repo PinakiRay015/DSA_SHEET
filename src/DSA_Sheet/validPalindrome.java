03f9b8a0af400f2c2e1094bfc2eb7eda14f4b638
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-16 02:22:39 +0530

    DSA SHEET

---
+++ src/DSA_Sheet/validPalindrome.java
@@ -0,0 +1,30 @@
+//java program to check whether a string is palindrome or not
+package DSA_Sheet;
+
+public class validPalindrome {
+    public static boolean checkValidation(String str)
+    {
+        int length = str.length();
+        StringBuilder newStr = new StringBuilder("");
+        for(int i=0 ; i<length ; i++)
+        {
+            if(Character.isLetter(str.charAt(i)) || Character.isDigit(str.charAt(i)))
+            {
+                newStr.append(str.charAt(i));
+            }
+        }
+
+        for(int i = 0 ; i<newStr.length()/2 ; i++)
+        {
+            if(newStr.charAt(i) == newStr.charAt(newStr.length()-1-i))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String str = new String("racecar");
+        System.out.println(checkValidation(str));
+    }
+}
