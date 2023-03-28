9c3e05cc895aa4bc2753a9fd63a0bcdfd5587e50
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-03-28 17:29:12 +0530

    DSA SHEET

---
+++ src/Array/trappingRainWater.java
@@ -0,0 +1,36 @@
+//java program to calculate the trapped rain water
+package Array;
+
+public class trappingRainWater {
+    public static int waterTrapped(int []height)
+    {
+        int n = height.length;
+        int []leftMax = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1 ; i<leftMax.length ; i++)
+        {
+            leftMax[i] = Math.max(height[i] , leftMax[i-1]);
+        }
+        
+        int []rightMax = new int[n];
+        rightMax[n-1] = height[n-1];
+
+        for(int i = n-2 ; i>=0 ; i--)
+        {
+            rightMax[i] = Math.max(height[i] , rightMax[i+1]);
+        }
+
+        int trappedWater = 0;
+        for(int i=0 ; i<height.length ; i++)
+        {
+            int waterlevel = Math.min(leftMax[i] , rightMax[i]);
+            trappedWater += waterlevel - height[i];
+        }
+
+        return trappedWater;
+    }
+    public static void main(String[] args) {
+        int []height = {4,2,0,6,3,2,5};
+        System.out.println(waterTrapped(height));
+    }
+}
